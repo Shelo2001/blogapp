fafa386673a0ff86e11e2605c21dd6ce06ffb51c
shelo2001 <sheliaakaki47@gmail.com>
2025-02-15 15:01:42 +0400

    added admin login middleware

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ Route::view('example-auth','example-auth');
 
 //admin routes
 Route::prefix('admin')->name('admin.')->group(function(){
-    Route::middleware([])->group(function () {
+    Route::middleware(['guest'])->group(function () {
         Route::controller(AuthController::class)->group(function(){
             Route::get('/login', 'loginForm')->name('login');
             Route::post('/login', 'loginHandler')->name('login_handler');
@@ -21,7 +21,7 @@ Route::prefix('admin')->name('admin.')->group(function(){
         });
     });
 
-    Route::middleware([])->group(function () {
+    Route::middleware(['auth'])->group(function () {
         Route::controller(AdminController::class)->group(function(){
             Route::get('/dashboard','adminDashboard')->name('dashboard');
             Route::post('/logout','logoutHandler' )->name('logout');
